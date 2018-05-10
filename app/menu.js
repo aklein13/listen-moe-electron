a6caee5e09c1094b8730ed516890c481cc4b1f68
Arkadiusz Klein <arek.klein@gmail.com>
2018-05-10 12:15:23 +0200

    removed more menus

--- app/menu.js
+++ app/menu.js
@@ -57,67 +57,9 @@ export default class MenuBuilder {
         }
       ]
     };
-    const subMenuEdit = {
-      label: 'Edit',
-      submenu: [
-        {label: 'Undo', accelerator: 'Command+Z', selector: 'undo:'},
-        {label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:'},
-        {type: 'separator'},
-        {label: 'Cut', accelerator: 'Command+X', selector: 'cut:'},
-        {label: 'Copy', accelerator: 'Command+C', selector: 'copy:'},
-        {label: 'Paste', accelerator: 'Command+V', selector: 'paste:'},
-        {label: 'Select All', accelerator: 'Command+A', selector: 'selectAll:'}
-      ]
-    };
-    const subMenuViewDev = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Reload', accelerator: 'Command+R', click: () => {
-            this.mainWindow.webContents.reload();
-          }
-        },
-        {
-          label: 'Toggle Full Screen', accelerator: 'Ctrl+Command+F', click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          }
-        },
-        {
-          label: 'Toggle Developer Tools', accelerator: 'Alt+Command+I', click: () => {
-            this.mainWindow.toggleDevTools();
-          }
-        }
-      ]
-    };
-    const subMenuViewProd = {
-      label: 'View',
-      submenu: [
-        {
-          label: 'Toggle Full Screen', accelerator: 'Ctrl+Command+F', click: () => {
-            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-          }
-        }
-      ]
-    };
-    const subMenuWindow = {
-      label: 'Window',
-      submenu: [
-        {label: 'Minimize', accelerator: 'Command+M', selector: 'performMiniaturize:'},
-        {label: 'Close', accelerator: 'Command+W', selector: 'performClose:'},
-        {type: 'separator'},
-        {label: 'Bring All to Front', selector: 'arrangeInFront:'}
-      ]
-    };
-
-    const subMenuView = process.env.NODE_ENV === 'development'
-      ? subMenuViewDev
-      : subMenuViewProd;
 
     return [
       subMenuAbout,
-      subMenuEdit,
-      subMenuView,
-      subMenuWindow,
     ];
   }
 
@@ -132,33 +74,6 @@ export default class MenuBuilder {
         accelerator: 'Ctrl+W',
         click: () => {
           this.mainWindow.close();
-        }
-      }]
-    }, {
-      label: '&View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: '&Reload',
-        accelerator: 'Ctrl+R',
-        click: () => {
-          this.mainWindow.webContents.reload();
-        }
-      }, {
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click: () => {
-          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
-        }
-      }, {
-        label: 'Toggle &Developer Tools',
-        accelerator: 'Ctrl+I',
-        click: () => {
-          this.mainWindow.toggleDevTools();
-        }
-      }] : [{
-        label: 'Toggle &Full Screen',
-        accelerator: 'F11',
-        click: () => {
-          this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
         }
       }]
     },
